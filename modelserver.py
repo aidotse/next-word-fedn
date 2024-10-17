964df184eb1a0655c918aa6f437c34c24dfb32e0
ChristopherJernberg <121690200+ChristopherJernberg@users.noreply.github.com>
2024-10-17 16:00:38 +0200

    added working LSTM model to load and compare with GRU

--- modelserver.py
+++ modelserver.py
@@ -77,4 +77,4 @@ model_type = 'gru_onehot'
 load_model(model_type)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- train/models.py
+++ train/models.py
@@ -1,29 +1,44 @@
 import torch.nn as nn
 
 class NextWordLSTM(nn.Module):
-    def __init__(self, vocab_size, embed_size, hidden_size, num_layers):
+    def __init__(self, vocab_size, embed_size, hidden_size, num_layers, repetition_penalty=1.0):
         super(NextWordLSTM, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, vocab_size)
+        self.repetition_penalty = repetition_penalty
     
     def forward(self, x):
         x = self.embedding(x)
         lstm_out, _ = self.lstm(x)
-        lstm_out = lstm_out[:, -1, :]
+        lstm_out = lstm_out[:, -1, :]  # Take the output of the last LSTM cell
         out = self.fc(lstm_out)
+        
+        if self.repetition_penalty != 1.0:
+            for i in range(out.size(0)):
+                for token in x[i]:
+                    out[i, token] /= self.repetition_penalty
+        
         return out
 
 class NextWordGRU(nn.Module):
-    def __init__(self, vocab_size, embed_size, hidden_size, num_layers):
+    def __init__(self, vocab_size, embed_size, hidden_size, num_layers, dropout_prob=0.3, repetition_penalty=1.0):
         super(NextWordGRU, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.gru = nn.GRU(embed_size, hidden_size, num_layers, batch_first=True)
+        self.dropout = nn.Dropout(dropout_prob)
         self.fc = nn.Linear(hidden_size, vocab_size)
-
+        self.repetition_penalty = repetition_penalty
+    
     def forward(self, x):
         x = self.embedding(x)
         gru_out, _ = self.gru(x)
-        gru_out = gru_out[:, -1, :]
+        gru_out = self.dropout(gru_out[:, -1, :]) 
         out = self.fc(gru_out)
+        
+        if self.repetition_penalty != 1.0:
+            for i in range(out.size(0)):
+                for token in x[i]:
+                    out[i, token] /= self.repetition_penalty
+        
         return out
